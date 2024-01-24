b79ae5dc8c71250153c09b06b7bca0042a8ae67d
Bart Oosten <bartoosten@gmail.com>
2024-01-24 13:39:05 +0100

    add: fonts and update global css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				"omnes-regular": ["Omnes-Regular", "sans-serif"],
+				"omnes-light": ["Omnes-Light", "sans-serif"],
+				"omnes-medium": ["Omnes-Medium", "sans-serif"],
+				"omnes-semibold": ["Omnes-SemiBold", "sans-serif"],
+				"source-sans-pro-regular": ["SourceSansPro-Regular", "sans-serif"],
+				"source-sans-pro-bold": ["SourceSansPro-Bold", "sans-serif"],
+				"source-sans-pro-light": ["SourceSansPro-Light", "sans-serif"],
+				"source-sans-pro-semibold": ["SourceSansPro-Semibold", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
